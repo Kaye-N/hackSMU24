34a82b3071a4b0b20fb075960f7f7964a69e50eb
RedShadow30 <148815559+RedShadow30@users.noreply.github.com>
2024-10-06 13:15:05 -0500

    Added the buttons, background img, and question

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,36 @@
 import { useEffect, useState, Image } from 'react'
 import './App.css'
+import backgroundImage from '../public/bkgd.png'
 
 function App() {
   
   const [questions, setQuestions] = useState([])
 
   useEffect(() => {
-    fetch("/api/questions")
+    fetch("http://localhost:3001/api/questions")
     .then(response => response.json())
     .then(data => setQuestions(data))
     .catch(err => console.log(err));     
   }, [])
 
-
-
   return (
-    <>     
-
+    <div className='app-container' style={{ backgroundImage: `url(${backgroundImage})` }}>     
       {/* Questions - useState when component renders use setState*/}
       {questions.length === 0 ? (
       <p>Loading...</p>
     ) : (
       questions.map((question, i) => (
-        <div key={i}>
+        <div key={i} className='question-container'>
           <p>{question.questions}</p> {/* Access the question */}
           <ul>
             {question.answers.map((answer, idx) => (
-              <li key={idx}>{answer}</li>
+              <button>{answer}</button>
             ))}
           </ul>
         </div>
       ))
     )}
-    </>
+    </div>
   )
 }
 
